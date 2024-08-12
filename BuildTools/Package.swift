a94c18a3be44222a47aecf6a023eb3802cefdbd5
awilliams88 <arpitwilliams88@gmail.com>
2024-08-12 20:28:40 +0530

    Installed package for composable architecture.

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
     name: "BuildTools",
     dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint", exact: "0.55.1"),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", exact: "0.51.5")
+        .package(url: "https://github.com/realm/SwiftLint", exact: "0.56.1"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", exact: "0.54.3")
     ],
     targets: [.target(name: "BuildTools", path: "")]
 )
