8325cd03fcd703965b1ae5c4a650b92f0f3eda61
awilliams88 <arpitwilliams88@gmail.com>
2024-08-13 21:07:35 +0530

    Added view store for feature to add book

---
+++ BookHive/Features/AddBook/AddBookStore.swift
@@ -0,0 +1,83 @@
+//
+// AddBookStore.swift
+// Created by Arpit Williams on 13/08/24.
+// Copyright (c) 2024 StarKnights Technologies
+
+import ComposableArchitecture
+import Foundation
+
+@Reducer
+struct AddBookStore {
+
+  @Dependency(\.dependencies)
+  var dependencies
+
+  @ObservableState
+  struct State: Equatable {
+    let id: Int
+    var title = ""
+    var author = ""
+    var description = ""
+    var publishedDate = Date()
+
+    var publishedISODate: String {
+      let formatter = ISO8601DateFormatter()
+      formatter.formatOptions.insert(.withFractionalSeconds)
+      return formatter.string(from: publishedDate)
+    }
+  }
+
+  enum Action: BindableAction {
+    case createBook
+    case saveBook(Book)
+    case dismiss
+    case binding(BindingAction<State>)
+  }
+
+  @Dependency(\.dismiss) var dismiss
+
+  var body: some Reducer<State, Action> {
+    BindingReducer()
+    Reduce { state, action in
+      switch action {
+
+      case .createBook:
+        guard state.title.isEmpty == false,
+              state.author.isEmpty == false
+        else { return .none }
+
+        let book = Book(
+          id: state.id, title: state.title,
+          author: state.author, description: state.description,
+          cover: "", publicationDate: state.publishedISODate
+        )
+        return .send(.saveBook(book))
+
+      case .dismiss:
+        return .run { _ in
+          await self.dismiss(animation: .default)
+        }
+
+      case .binding, .saveBook:
+        return .none
+      }
+    }
+  }
+}
+
+// MARK: Load Store
+
+extension AddBookStore {
+
+  static func loadStore(id: Int) -> StoreOf<Self> {
+    .init(initialState: State(id: id)) {
+      AddBookStore()
+    }
+  }
+
+  static func mockStore() -> StoreOf<Self> {
+    .init(initialState: State(id: 100)) {
+      AddBookStore()
+    }
+  }
+}
---
+++ BookHive/Features/AddBook/AddBookView.swift
@@ -0,0 +1,83 @@
+//
+// AddBookView.swift
+// Created by Arpit Williams on 13/08/24.
+// Copyright (c) 2024 StarKnights Technologies
+
+import ComposableArchitecture
+import SwiftUI
+
+struct AddBookView: View {
+
+  @Perception.Bindable var store: StoreOf<AddBookStore>
+  init(store: StoreOf<AddBookStore>) {
+    self.store = store
+  }
+
+  var body: some View {
+    WithPerceptionTracking {
+      NavigationView {
+        Form {
+          textInputs
+          dateInput
+          buttons
+        }
+        .navigationBarTitle("Add Book 📚")
+      }
+    }
+  }
+
+  var textInputs: some View {
+    Section(
+      header: Text("Book Details")
+        .font(.callout).fontWeight(.semibold)
+    ) {
+      TextField("Title", text: $store.title)
+      TextField("Author", text: $store.author)
+      TextField("Description", text: $store.description)
+    }
+  }
+
+  var dateInput: some View {
+    Section(
+      header: Text("Published Date")
+        .font(.callout).fontWeight(.semibold)
+    ) {
+      DatePicker(
+        "Pick a Date",
+        selection: $store.publishedDate,
+        displayedComponents: .date
+      )
+    }
+  }
+
+  var buttons: some View {
+    VStack(spacing: 16) {
+      Button(
+        action: { store.send(.createBook) },
+        label: {
+          Text("Save Book")
+            .textCase(.uppercase)
+            .font(.body.weight(.semibold))
+        }
+      )
+
+      Divider()
+
+      Button(
+        action: { store.send(.dismiss) },
+        label: {
+          Text("CANCEL")
+            .textCase(.uppercase)
+            .font(.callout.weight(.semibold))
+            .foregroundColor(Color(.systemPink))
+        }
+      )
+    }.frame(maxWidth: .infinity, alignment: .center)
+  }
+}
+
+#Preview {
+  AddBookView(
+    store: AddBookStore.mockStore()
+  )
+}
