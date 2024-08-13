5593dbf1786055bb85d0a2bf363a3b983ede1f24
awilliams88 <arpitwilliams88@gmail.com>
2024-08-14 00:49:29 +0530

    Added unit tests for add book store.

---
+++ BookHiveTests/AddBookStoreTests.swift
@@ -0,0 +1,30 @@
+//
+// AddBookStoreTests.swift
+// Created by Arpit Williams on 14/08/24.
+// Copyright (c) 2024 StarKnights Technologies
+
+@testable import BookHive
+import ComposableArchitecture
+import XCTest
+
+final class AddBookStoreTests: XCTestCase {
+
+  @MainActor
+  func testReducer() async {
+    let store = TestStore(initialState: AddBookStore.State(id: 0)) {
+      AddBookStore()
+    }
+
+    // Create Book
+    await store.send(.binding(.set(\.title, "title"))) {
+      $0.title = "title"
+      $0.publishedDate = Date(timeIntervalSince1970: 0)
+    }
+    await store.send(.binding(.set(\.author, "author"))) {
+      $0.author = "author"
+    }
+    await store.send(.createBook)
+    await store.receive(\.saveBook)
+    await store.receive(\.dismiss)
+  }
+}
