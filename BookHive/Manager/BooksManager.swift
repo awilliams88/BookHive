5ab2c3f595f31e6d1953c4cfe232ee1dfb6be289
awilliams88 <arpitwilliams88@gmail.com>
2024-08-13 12:57:47 +0530

    Added protocol for books manager

---
+++ BookHive/Manager/BooksManager.swift
@@ -0,0 +1,75 @@
+//
+// BooksManager.swift
+// Created by Arpit Williams on 12/08/24.
+// Copyright (c) 2024 StarKnights Technologies
+
+import Foundation
+
+final class BooksManager: BooksManagerProtocol {
+
+  let bookService: BookServiceProtocol
+  init(bookService: any BookServiceProtocol) {
+    self.bookService = bookService
+  }
+
+  func create(_ book: Book) async throws {
+    var books = try await getLocalBooks()
+    books.append(book)
+    try await updateLocalBooks(books)
+  }
+
+  func update(_ book: Book) async throws {
+    var books = try await getLocalBooks()
+    guard let index = books.firstIndex(where: { $0.id == book.id }) else { return }
+    books[index] = book
+    try await updateLocalBooks(books)
+  }
+
+  func remove(_ book: Book) async throws {
+    var books = try await getLocalBooks()
+    guard let index = books.firstIndex(where: { $0.id == book.id }) else { return }
+    books.remove(at: index)
+    try await updateLocalBooks(books)
+  }
+
+  func getBook(with id: Int) async throws -> Book {
+    let books = try await getAllBooks()
+    guard let book = books.first(where: { $0.id == id }) else {
+      throw BookError.invalidBookId
+    }
+    return book
+  }
+
+  func getAllBooks() async throws -> [Book] {
+    let localBooks = try await getLocalBooks()
+    let remoteBooks = try await bookService.getAllBooks()
+    return localBooks + remoteBooks
+  }
+}
+
+// MARK: Private Helpers
+
+private extension BooksManager {
+
+  func getLocalBooks() async throws -> [Book] {
+    try await Task<[Book], Error> {
+      let fileUrl = try Self.booksUrl()
+      guard let data = try? Data(contentsOf: fileUrl) else { return [] }
+      return try JSONDecoder().decode([Book].self, from: data)
+    }.value
+  }
+
+  func updateLocalBooks(_ books: [Book]) async throws {
+    try await Task {
+      let data = try JSONEncoder().encode(books)
+      let fileUrl = try Self.booksUrl()
+      try data.write(to: fileUrl)
+    }.value
+  }
+
+  static func booksUrl() throws -> URL {
+    try FileManager.default
+      .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+      .appendingPathComponent("books.data")
+  }
+}
---
+++ BookHive/Manager/BooksManagerProtocol.swift
@@ -0,0 +1,15 @@
+//
+// BooksManagerProtocol.swift
+// Created by Arpit Williams on 12/08/24.
+// Copyright (c) 2024 StarKnights Technologies
+
+import Foundation
+
+protocol BooksManagerProtocol {
+  var bookService: BookServiceProtocol { get }
+  func create(_ book: Book) async throws
+  func update(_ book: Book) async throws
+  func remove(_ book: Book) async throws
+  func getAllBooks() async throws -> [Book]
+  func getBook(with id: Int) async throws -> Book
+}
