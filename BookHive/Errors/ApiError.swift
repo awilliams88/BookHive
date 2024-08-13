65d72dafd68c68ea4bebcddf4d5bab1db0d30288
awilliams88 <arpitwilliams88@gmail.com>
2024-08-13 12:58:01 +0530

    Added Errors

---
+++ BookHive/Errors/ApiError.swift
@@ -0,0 +1,10 @@
+//
+// ApiError.swift
+// Created by Arpit Williams on 12/08/24.
+// Copyright (c) 2024 StarKnights Technologies
+
+import Foundation
+
+enum ApiError: Error {
+  case invalidUrl
+}
---
+++ BookHive/Errors/BookError.swift
@@ -0,0 +1,10 @@
+//
+// BookError.swift
+// Created by Arpit Williams on 13/08/24.
+// Copyright (c) 2024 StarKnights Technologies
+
+import Foundation
+
+enum BookError: Error {
+  case invalidBookId
+}
